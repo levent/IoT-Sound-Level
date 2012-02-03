d66b4d22c99ffa02173d2da6653d665b2f2d7176
Levent Ali <lebreeze@gmail.com>
2012-02-03 16:15:08 +0000

    Initial Commit

---
+++ IoT Sound Level/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IoT Sound Level
+//
+//  Created by Levent Ali on 03/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ IoT Sound Level/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  IoT Sound Level
+//
+//  Created by Levent Ali on 03/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
